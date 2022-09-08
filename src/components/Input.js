504f47c7602db15f046664dd5eb8a8b7e7e3099d
Aleksandr Panarin <panarin0290@gmail.com>
2022-09-08 22:58:10 +0300

    add input

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export function Input({ children }){
-    function submitHandler(e){
-        e.preventDefault()
+export function Input({ className, children, validation }){
+    const [value, setValue] = useState('')
+    
+    function onChange(e){
+        let validValue = e.target.value
+        if (validation !== undefined) {
+            validValue = validation(validValue)
+        }
+        setValue(validValue)
     }
     return (
-        <>  
-        <form onSubmit={submitHandler}>
-            <span>{children}</span>
+        <div className={'flex '+className}>
+            <div className='w-[100px]'>
+                <span>{ children }</span>
+            </div>
+            
             <input 
                 type='text'
-                className='border ml-3 outline-0 px-3'
+                className='border ml-3 outline-0 px-3 mb-3'
+                onChange={onChange}
+                value={value}
             ></input>
-        </form>
-        </>
+        </div>
+        
     )
 }
